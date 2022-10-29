0babf8873d3a6e44f3dce396e6c7c7d63a08b7ba
kalkuk <115346296+kalkuk@users.noreply.github.com>
2022-10-29 10:22:35 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,5 @@
+function checkTemp(kateltemp)
+{
+if (kateltemp>=0 && kateltemp <=20) {return -1}
+else if (kateltemp>=21 && kateltemp <=40) {return 0}
+else if (kateltemp>=41 && kateltemp <=60) {return 1}}
